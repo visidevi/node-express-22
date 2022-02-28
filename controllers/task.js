7aa3971e69715a45e7d76234dca5e0c0fc39f8b9
visidevi <visidevi1@gmail.com>
2022-02-28 01:14:13 -0300

    make it better

--- controllers/task.js
+++ controllers/task.js
@@ -19,19 +19,63 @@ async function createTask(req, res) {
 
 async function getTasks(req, res) {
   try {
-    const task = await Task.find();
-    if (!task) {
+    const tasks = await Task.find({ completed: false }).sort({
+      created_at: -1,
+    });
+    if (!tasks) {
       res.status(404).send({ message: "No hay tareas" });
+    } else {
+      res.status(200).json(tasks);
+    }
+  } catch (error) {
+    res.status(500).send({ message: `Error al obtener las tareas ${error}` });
+  }
+}
+async function getTask(req, res) {
+  try {
+    console.log(req.params.id);
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      res.status(404).send({ message: "No hay tarea" });
     } else {
       res.status(200).json(task);
     }
+  } catch (error) {
+    res.status(500).send(error);
   }
-  catch (error) {
-    res.status(500).send({ message: `Error al obtener las tareas ${error}` });
+}
+async function updateTask(req, res) {
+  try {
+    console.log(req.params.id);
+    const params = req.body;
+    const task = await Task.findByIdAndUpdate(req.params.id, params);
+    if (!task) {
+      res.status(404).send({ message: "No hay tarea" });
+    } else {
+      res.status(200).json(task);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+async function deleteTask(req, res) {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      res.status(404).send({ message: "No se pudo eliminar" });
+    } else {
+      res.status(200).json(task);
+    }
+  } catch (error) {
+    res.status(500).send(error);
   }
 }
 
 module.exports = {
   createTask,
   getTasks,
+  getTask,
+  updateTask,
+  deleteTask,
 };
--- routes/task.js
+++ routes/task.js
@@ -3,4 +3,8 @@ const TaskController = require("../controllers/task.js");
 const api = express.Router();
 api.post("/task", TaskController.createTask);
 api.get("/task", TaskController.getTasks);
+api.get("/task/:id", TaskController.getTask);
+api.put("/task/:id", TaskController.updateTask);
+api.delete("/task/:id", TaskController.deleteTask);
+
 module.exports = api;
