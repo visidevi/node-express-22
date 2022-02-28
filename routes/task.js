3ce22594b98f7c1d22ebf310dbe51cf8ba226f1a
visidevi <visidevi1@gmail.com>
2022-02-28 01:26:12 -0300

    make it better

--- routes/task.js
+++ routes/task.js
@@ -2,7 +2,7 @@ const express = require("express");
 const TaskController = require("../controllers/task.js");
 const api = express.Router();
 api.post("/task", TaskController.createTask);
-api.get("/task", TaskController.getTasks);
+api.get("/tasks", TaskController.getTasks);
 api.get("/task/:id", TaskController.getTask);
 api.put("/task/:id", TaskController.updateTask);
 api.delete("/task/:id", TaskController.deleteTask);
